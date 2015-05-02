43cd41c142e99a5e62728daa86f3ea0d64338646
Ben Westrate <benjamin.westrate@icfi.com>
2015-05-01 20:52:49 -0500

    small tweeks

--- bootstrap-new-system.sh
+++ bootstrap-new-system.sh
@@ -7,15 +7,6 @@ pushd .
 mkdir -p $dev
 cd $dev
 
-echo 'Enter new hostname of the machine (e.g. macbook-paulmillr)'
-  read hostname
-  echo "Setting new hostname to $hostname..."
-  scutil --set HostName "$hostname"
-  compname=$(sudo scutil --get HostName | tr '-' '.')
-  echo "Setting computer name to $compname"
-  scutil --set ComputerName "$compname"
-  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
-
 pub=$HOME/.ssh/id_rsa.pub
 echo 'Checking for SSH key, generating one if it does not exist...'
   [[ -f $pub ]] || ssh-keygen -t rsa
